ad5622ecd10cccd82073991642a04c0309f049bb
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-03-28 23:36:10 -0600

    Removed debugging. Playing around with hash change, probably need a plugin.

--- awesome.js
+++ awesome.js
@@ -4,9 +4,6 @@ $(document).ready(function(){
     var id = $(this).attr('href');
     var section = $(id).clone().wrap('<div>').parent().html();
     var active = $('.active_container section');
-    console.log(id)
-    console.log(section)
-    console.log(active)
     active.fadeOut(function(){
       $('.inactive_container').append(active.clone().wrap('<div>').parent().html());
       $('.active_container').html(section).find('section').fadeIn();
@@ -14,7 +11,11 @@ $(document).ready(function(){
     })
     $(this).parents('ul').find('li').removeClass('active');
     $(this).parent().addClass('active');
-    
     return false;
    });
 });
+
+//$(document).bind('window.onhashchange', function(event){
+  //console.log('shaboom')
+  //console.log(event)
+//});
