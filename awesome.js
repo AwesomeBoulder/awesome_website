7088dc8827403f262f6dfd9315e824a936b97f09
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-04-02 10:41:15 -0600

    Installed GoogleAnalytics

--- awesome.js
+++ awesome.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
   $('.apply').tooltip({placement: 'right'});
+  $('.apply').live('click', function(){
+    _gaq.push(['_trackEvent', 'apply', 'clicked']);
+  });
   $('.nav a').click(function(){
     var id = $(this).attr('href');
     var section = $(id).clone().wrap('<div>').parent().html();
     var active = $('.active_container section');
+    _gaq.push(['_trackEvent', id, 'clicked']);
     active.fadeOut(function(){
       $('.inactive_container').append(active.clone().wrap('<div>').parent().html());
       $('.active_container').html(section).find('section').fadeIn();
       $('.apply').tooltip({placement: 'right'});
-    })
+    });
     $(this).parents('ul').find('li').removeClass('active');
     $(this).parent().addClass('active');
     return false;
